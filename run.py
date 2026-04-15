cd14a7cb9f72465c82fa3756e4d987065a0374dd
root <root@apiSafeClaim.local>
2026-04-15 08:06:05 +0000

    Set debug=False

--- run.py
+++ run.py
@@ -1,9 +1,4 @@
 from app import create_app
 app = create_app()
 if __name__ == "__main__":
-<<<<<<< HEAD
     app.run(host="0.0.0.0", port=5000, debug=False)
-=======
-    app.run(host="0.0.0.0", port=5000, debug=True)
-    
->>>>>>> b009b1e (creazione api richieste)
